c0e53fab6482ce3f59c40b3283890b685d76e8e0
Matthew Boyea <contact@mboyea.com>
2026-03-05 22:08:51 -0600

    auto-import ./utils into flake pkgs

--- flake.nix
+++ flake.nix
@@ -7,38 +7,48 @@
   outputs = { self, nixpkgs, flake-utils, ... }: let
     name = "rift-drive";
     version = "0.0.0";
-    utils = flake-utils;
-  in utils.lib.eachDefaultSystem (
+    flakeUtils = flake-utils;
+  in flakeUtils.lib.eachDefaultSystem (
     system: let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = (import nixpkgs { inherit system; }).extend (final: prev: {
+        utils = (import "${self}/utils" { pkgs = final; }).packages;
+      });
     in rec {
+      legacyPackages = pkgs;
       packages = {
-        help = pkgs.callPackage ./utils/run.nix {
+        help = pkgs.utils.run {
            name = "${name}-help-${version}";
            target = ./scripts/help.sh;
         };
       };
       apps = {
-        help = utils.lib.mkApp { drv = packages.help; };
+        help = flakeUtils.lib.mkApp { drv = packages.help; };
         default = apps.help;
       };
       devShells = let
         run-alias = pkgs.writeShellScriptBin "run" ''
-          script_name="$1" && shift
-          exec nix run .#"$script_name" -- "$@"
+          TARGET_SCRIPT=''${1:-help}
+          BIN_NAME="${name}-$TARGET_SCRIPT-${version}"
+          if command -v "$BIN_NAME" >/dev/null 2>&1; then
+            exec "$BIN_NAME" "''${@:2}"
+          else
+            exec nix run .#"$TARGET_SCRIPT" -- "''${@:2}"
+          fi
         '';
+        bin-scripts = pkgs.symlinkJoin { name = "${name}-${version}-scripts"; paths = builtins.attrValues packages; };
       in {
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            run-alias
-            awscli2
-            minio
+          packages = [
+            run-alias                 # run <script>
+            bin-scripts               # enable run-alias to derive scripts without re-evaluation (fast!)
+            pkgs.nix-prefetch-docker  # dockerTools.pullImage < nix-prefetch-docker --quiet --image-name _ --image-tag _ --image-digest _
           ];
           shellHook = ''
             # load .env
             source ./scripts/load-env.sh
             # suppress warning about dirty git for nix commands
             export NIX_CONFIG="warn-dirty = false"
+            echo -e "\033[1;32mSUCCESSFULLY LOADED DEVSHELL FOR ${name}-${version}\033[0m"
           '';
         };
       };
---
+++ utils/default.nix
@@ -0,0 +1,68 @@
+{ pkgs }: let
+  toCamelCase = str: let
+    parts = pkgs.lib.splitString "-" str;
+    upperFirst = word: (pkgs.lib.toUpper (builtins.substring 0 1 word)) + (builtins.substring 1 (builtins.stringLength word) word);
+  in if (builtins.length parts) < 2
+    then str
+    else let
+      first = pkgs.lib.head parts;
+      rest = map upperFirst (pkgs.lib.tail parts);
+    in builtins.concatStringsSep "" ([first] ++ rest);
+in {
+  packages = pkgs.lib.mapAttrs'
+    (fileName: _: let
+      baseName = pkgs.lib.removeSuffix ".nix" fileName;
+      camelName = toCamelCase baseName;
+      importedFile = import (./. + "/${fileName}");
+      args = if builtins.isFunction importedFile 
+        then builtins.functionArgs importedFile 
+        else {};
+      value = if args ? pkgs
+        then (overrides: importedFile (({
+          inherit pkgs;
+          stdenv = pkgs.stdenv;
+        } // (builtins.intersectAttrs args pkgs)) // overrides))
+        else importedFile;
+    in pkgs.lib.nameValuePair camelName value)
+    (pkgs.lib.filterAttrs (name: type: 
+      type == "regular" && pkgs.lib.hasSuffix ".nix" name && name != "default.nix"
+    ) (builtins.readDir ./.));
+}
+
+# { pkgs }: let
+#   toCamelCase = s: let
+#       parts = pkgs.pkgs.lib.splitString "-" s;
+#       upperFirst = word: 
+#         (pkgs.pkgs.lib.toUpper (builtins.substring 0 1 word)) + (builtins.substring 1 (builtins.stringLength word) word);
+#     in if (builtins.length parts) < 2 then s 
+#     else let
+#         first = pkgs.pkgs.lib.head parts;
+#         rest = map upperFirst (pkgs.pkgs.lib.tail parts);
+#     in builtins.concatStringsSep "" ([first] ++ rest);
+# in {
+#   packages = pkgs.pkgs.lib.mapAttrs' (fileName: _: 
+#     let
+#       baseName = pkgs.pkgs.lib.removeSuffix ".nix" fileName;
+#       camelName = toCamelCase baseName;
+#       importedFile = import (./. + "/${fileName}");
+#       
+#       args = if builtins.isFunction importedFile 
+#              then builtins.functionArgs importedFile 
+#              else {};
+# 
+#       # The wrapper that injects pkgs/stdenv into your "run.nix" style files
+#       value = if args ? pkgs then
+#         (overrides: importedFile (({
+#           inherit pkgs;
+#           stdenv = pkgs.stdenv;
+#         } // (builtins.intersectAttrs args pkgs)) // overrides))
+#       else
+#         importedFile;
+# 
+#     in
+#     pkgs.pkgs.lib.nameValuePair camelName value
+#   ) pkgs.pkgs.lib.filterAttrs (name: type: 
+#     type == "regular" && pkgs.pkgs.lib.hasSuffix ".nix" name && name != "default.nix"
+#   ) (builtins.readDir ./.);
+# }
+
