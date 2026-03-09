98adf5482f8529999667875e3f903b2222bace24
Matthew Boyea <contact@mboyea.com>
2026-03-09 14:18:39 -0500

    renamed utils to lib and implemented lazy loading such that lib is added to pkgs.lib and can reference eachother

--- flake.nix
+++ flake.nix
@@ -5,37 +5,40 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils, ... }: let
-    name = "rift-drive";
+    pname = "rift-drive";
     version = "0.0.0";
-    flakeUtils = flake-utils;
-  in flakeUtils.lib.eachDefaultSystem (
+    utils = flake-utils;
+  in utils.lib.eachDefaultSystem (
     system: let
       pkgs = (import nixpkgs { inherit system; }).extend (final: prev: {
-        utils = (import "${self}/utils" { pkgs = final; }).packages;
+        lib = prev.lib // import ./lib {
+          pkgs = final;
+          lib = prev.lib;
+	};
       });
     in rec {
       legacyPackages = pkgs;
       packages = {
-        help = pkgs.utils.run {
-           name = "${name}-help-${version}";
+        help = pkgs.lib.run {
+           name = "${pname}-help-${version}";
            target = ./scripts/help.sh;
         };
       };
       apps = {
-        help = flakeUtils.lib.mkApp { drv = packages.help; };
+        help = utils.lib.mkApp { drv = packages.help; };
         default = apps.help;
       };
       devShells = let
         run-alias = pkgs.writeShellScriptBin "run" ''
           TARGET_SCRIPT=''${1:-help}
-          BIN_NAME="${name}-$TARGET_SCRIPT-${version}"
+          BIN_NAME="${pname}-$TARGET_SCRIPT-${version}"
           if command -v "$BIN_NAME" >/dev/null 2>&1; then
             exec "$BIN_NAME" "''${@:2}"
           else
             exec nix run .#"$TARGET_SCRIPT" -- "''${@:2}"
           fi
         '';
-        bin-scripts = pkgs.symlinkJoin { name = "${name}-${version}-scripts"; paths = builtins.attrValues packages; };
+        bin-scripts = pkgs.symlinkJoin { name = "${pname}-${version}-scripts"; paths = builtins.attrValues packages; };
       in {
         default = pkgs.mkShell {
           packages = [
@@ -48,10 +51,12 @@
             ENV_FILE=".env" source ./scripts/load-env.sh
             # suppress warning about dirty git for nix commands
             export NIX_CONFIG="warn-dirty = false"
-            echo -e "\033[1;32mSUCCESSFULLY LOADED DEVSHELL FOR ${name}-${version}\033[0m"
+            echo -e "\033[1;32mSUCCESSFULLY LOADED DEVSHELL FOR ${pname}-${version}\033[0m"
           '';
         };
       };
     }
-  );
+  ) // {
+    lib = import ./lib;
+  };
 }
---
+++ lib/default.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  lib ? pkgs.lib,
+}: let
+  toCamelCase = str: let
+    parts = lib.splitString "-" str;
+    upperFirst = word: (lib.toUpper (builtins.substring 0 1 word)) + (builtins.substring 1 (builtins.stringLength word) word);
+  in if (builtins.length parts) < 2
+    then str
+    else let
+      first = lib.head parts;
+      rest = map upperFirst (lib.tail parts);
+    in builtins.concatStringsSep "" ([first] ++ rest);
+in (lib.mapAttrs'
+  (fileName: _: let
+    baseName = lib.removeSuffix ".nix" fileName;
+    camelName = toCamelCase baseName;
+    importedFile = import (./. + "/${fileName}");
+    args = if builtins.isFunction importedFile 
+      then builtins.functionArgs importedFile 
+      else {};
+    value = if args ? pkgs
+      then (overrides: importedFile (({
+        inherit pkgs;
+        stdenv = pkgs.stdenv;
+      } // (builtins.intersectAttrs args pkgs)) // overrides))
+      else importedFile;
+  in lib.nameValuePair camelName value)
+  (lib.filterAttrs (name: type: 
+    type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix"
+  ) (builtins.readDir ./.))
+) // {
+  inherit toCamelCase;
+  strings = lib.strings // { inherit toCamelCase; };
+}
+
